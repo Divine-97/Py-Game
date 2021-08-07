088bc6eae767ee13045fd3f3811408c735f7e098
Divine-97 <kumwendadivine@gmail.com>
2021-08-07 19:01:44 +0000

    Add source code for bear_room

--- run.py
+++ run.py
@@ -15,4 +15,25 @@ def start():
     # else call game_over() function with the "reason" argument
         game_over("Don't you know how to type something properly?")
 
-start()   
+def bear_room():
+    print("\nThere is a bear here.")
+    print("Behind the bear is another door.")
+    print("The bear is eating tasty honey!")
+    print("What would you do? (1 or 2)")
+    print("1). Take the honey.")
+    print("2). Taunt the bear.")
+
+    answer = input(">")
+
+    if answer == "1":
+    # the player is dead!
+        game_over("The bear killed you.")
+    elif answer == "2":
+    # lead him to the diamond_room()
+        print("\nYour Good time, the bear moved from the door. You can go through it now!")
+        diamond_room()
+    else:
+    # else call game_over() function with the "reason" argument
+        game_over("Don't you know how to type a number?")
+start()  
+
